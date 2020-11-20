0979c877720d15a754948d63dd7d9aa225d5ac98
rme18422 <rme18422@gmail.com>
2020-11-20 07:52:55 +0500

    done a lot today

--- CodeSignal/CodilityLessons.cs
+++ CodeSignal/CodilityLessons.cs
@@ -10,22 +10,144 @@ namespace CodeSignal
     {
         public static void main()
         {
-
+            runTapeEquilibrium();
+            runFindMissingElement();
+            runFrogJumps();
+            runOddOccurencesInArray();
+            runBinaryGap();
+            runRotateArray();
             Console.ReadKey();
         }
+        public static void runTapeEquilibrium()
+        {
+            Random _random = new Random();
+            var arr = new int[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                arr[i] = _random.Next(-100000,10000);
+                //arr[i] = _random.Next(-100000,-1);
+                //arr[i] = _random.Next(1,10000);
+            }
+            arr = new int[] { -4, 3,3};
+            var result = tapeEquilibrium(arr);
+            var result2 = tapeEquilibrium2(arr);
+            Console.WriteLine(result+"..");
+            Console.WriteLine(result2 + "..");
 
+        }
+        public static int tapeEquilibrium(int[] A)
+        {
+            if (A.Length == 0) return 0;
+            if (A.Length == 1) return Math.Abs(A[1]);
+            var minSum = -1;
+            for (int i = 1; i < A.Length; i++)
+            {
+                var diff = Math.Abs(A.Take(i).Sum() - A.Skip(i).Sum());
+                if (minSum == -1) minSum = diff;
+                else if (diff < minSum) minSum = diff;
+            }
+            return minSum;
+        }
+
+        public static int tapeEquilibrium2(int[] A)
+        {
+            if (A.Length == 0) return 0;
+            if (A.Length == 1) return Math.Abs(A[1]);
+            var totalSum = Math.Abs(A.Sum());
+            var total = 0;
+            int? minSum = 0;
+            for (int i = 0; i < A.Length-1; i++)
+            {
+                total += A[i];
+                var tempTotalDiff = Math.Abs(Math.Abs(2 * total) - totalSum);
+                if (minSum == 0)
+                    minSum = tempTotalDiff;
+                else if (minSum > tempTotalDiff)
+                    minSum = tempTotalDiff;
+                Console.WriteLine(tempTotalDiff);
+            }
+            return Convert.ToInt32(minSum);
+        }
+        public static void runFindMissingElement()
+        {
+            var result = FindMissingElement(new int[] { 2, 3 });
+            //Console.WriteLine(result);
+        }
+        public static int FindMissingElement(int[] A)
+        {
+            if (A.Length == 0) return 1;
+            Array.Sort(A);
+            var next = A[0] == 1 ? 2 : 1;
+            for (int i = 1; i < A.Length; i++)
+            {
+                if (A[i] == next) next++;
+                else return next;
+            }
+            return next;
+        }
+        public static void runFrogJumps()
+        {
+            var result = frogJumps(10, 85, 10);
+            //Console.WriteLine(result);
+        }
+        public static int frogJumps(int X, int Y, int D)
+        {
+            var diff = Y - X;
+            var extraJumpFraction = diff % D;
+            if (extraJumpFraction == 0) return (Y - X) / D;
+            else return ((Y - X) / D) + 1;
+        }
+        public static void runOddOccurencesInArray()
+        {
+            var arr = new int[] { 9, 3, 9, 3, 9, 7, 9 };
+            var result = oddOccurencesInArray(arr);
+            //Console.WriteLine(result);
+        }
+        public static int oddOccurencesInArray(int[] A)
+        {
+            int res = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                res = res ^ A[i];
+            }
+            return res;
+        }
+        public static void runBinaryGap()
+        {
+            var result = binaryGap(1011);
+            //Console.WriteLine(result);
+        }
+        public static int binaryGap(int N)
+        {
+            var binary = Convert.ToString(N, 2);
+            var maxLength = 0;
+            var startIndex = -1;
+            var zeroCounter = 0;
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (startIndex != -1) zeroCounter++;
+                if (binary[i] == '1' && startIndex == -1) startIndex = i;
+                if (binary[i] == '1' && startIndex != -1)
+                {
+                    maxLength = maxLength >= zeroCounter ? maxLength : zeroCounter - 1;
+                    zeroCounter = 0;
+                }
+            }
+            return maxLength == -1 ? 0 : maxLength;
+        }
         public static void runRotateArray()
         {
-            var arr = new int[] { 1,2 };
+            var arr = new int[] { 1, 2 };
             var rotations = 3;
-            arr = rotateArray(arr, rotations);
+            var result = rotateArray(arr, rotations);
         }
         public static int[] rotateArray(int[] A, int K)
         {
             var length = A.Length;
             var B = new int[length];
+            if (length == 0) return A;
             if (length == K) return A;
-            if (length < K) K = K%length;
+            if (length < K) K = K % length;
             for (int i = 0; i < length; i++)
             {
                 var temp = A[i];
